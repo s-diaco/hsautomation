4389f1d4d511851f09c906c463be3824a0efc8dd
Diaco <dyaco8464@yahoo.com>
2024-04-18 13:39:03 +0330

    tests passed

---
+++ tests/test_hsadmin.py
@@ -0,0 +1,10 @@
+from time import sleep
+
+
+def test_play():
+    from hsautomation.bandleader import BandLeader
+
+    bandleader = BandLeader("db.csv")
+    bandleader.play()
+    sleep(10)
+    assert bandleader.currently_playing()
