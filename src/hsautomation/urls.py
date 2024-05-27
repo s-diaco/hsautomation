03d7e1a4117061cfff8bbb36a0b26e4d1c169804
diaco <dyaco8464@yahoo.com>
2024-05-27 10:35:54 +0330

    added urls for frontpage and loginpage

--- src/hsautomation/urls.py
+++ src/hsautomation/urls.py
@@ -1,4 +1,4 @@
-LOGINURL = ""
+LOGINURL = "http://192.168.3.193/sg23g/x4e205b34/Authentication/Login.aspx?ReturnUrl=%2fsg23g%2fx4e205b34%2f%3frnd%3d8db8b693bc0c32c%26page%3dLogistics%252FInventoryReports%252FQuantitativeReview%252FReview.aspx"
 UNAME = "johar"
 PASSWD = "1402"
-FRONTPAGE = ""
+FRONTPAGE = "http://192.168.3.193/sg23g/x4e205b34/?page=Logistics%2FInventoryReports%2FQuantitativeReview%2FReview.aspx&rnd=8dc7e38819fad54"
